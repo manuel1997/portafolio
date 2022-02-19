c079c1eb41d4a917beb8b89ebf57c806245a92c4
manuel1997 <morillo864@gmail.com>
2022-02-19 00:36:20 -0400

    seo2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,14 +17,12 @@ export class AppComponent implements OnInit {
       private meta: Meta 
       ) {
         this.title.setTitle('Ivan Morillo | Desarrollador de Aplicaciones Web');
-        this.meta.addTags([
-          {name: 'description', content: 'Hola soy ivan morillo, desarrollador de aplicaciones web, con tecnologías como Angular, Nodejs, Php, Mysql, Mongodb, entre otras.'},
-          {name: 'keywords', content: 'ivan morillo,desarrollador,web,programación,developer,angular,javascript'},
+        this.meta.updateTag({name: 'description', content: 'Hola soy ivan morillo, desarrollador de aplicaciones web, con tecnologías como Angular, Nodejs, Php, Mysql, Mongodb, entre otras.'}); 
+        this.meta.updateTag({name: 'keywords', content: 'ivan morillo,desarrollador,web,programación,developer,angular,javascript'});
 
-          {name: 'og:title', content: 'Ivan Morillo | Desarrollador de Aplicaciones Web'},
-          {name: 'og:description', content: 'Hola soy ivan morillo, desarrollador de aplicaciones web, con tecnologías como Angular, Nodejs, Php, Mysql, Mongodb, entre otras.'},
-          {name: 'og:image', content: 'assets/img/portafolio.png'},
-        ]);
+        this.meta.updateTag({name: 'og:title', content: 'Ivan Morillo | Desarrollador de Aplicaciones Web'});
+        this.meta.updateTag({name: 'og:description', content: 'Hola soy ivan morillo, desarrollador de aplicaciones web, con tecnologías como Angular, Nodejs, Php, Mysql, Mongodb, entre otras.'});
+        this.meta.updateTag({name: 'og:image', content: 'assets/img/portafolio.png'});
       }
 
   ngOnInit(): void {
